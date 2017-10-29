078746b6406454b96739febb2b1a17f8142875b4
Denis Weerasiri <ddweerasiri@gmail.com>
2017-10-29 14:25:51 +1100

    Enabled exit codes from the application-start.sh and before-install.sh

--- code-deploy-scripts/application-start.sh
+++ code-deploy-scripts/application-start.sh
@@ -4,4 +4,11 @@ echo "Running application-start.sh..."
 cd /home/ec2-user/ddweerasiri-website
 npm run start
 
-echo "Finished running application-start.sh..."
+if [ $? -eq 0 ]
+then
+  echo "Finished running application-start.sh..."
+  exit 0
+else
+  echo "Failed running application-start.sh..." >&2
+  exit 1
+fi
--- code-deploy-scripts/before-install.sh
+++ code-deploy-scripts/before-install.sh
@@ -17,4 +17,11 @@ npm -v
 cd /home/ec2-user/ddweerasiri-website
 npm install
 
-echo "Finished running before-install.sh..."
+if [ $? -eq 0 ]
+then
+  echo "Finished running before-install.sh..."
+  exit 0
+else
+  echo "Failed running before-install.sh..." >&2
+  exit 1
+fi
