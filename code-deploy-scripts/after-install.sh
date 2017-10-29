e753bd3da677ba041530fb90d9f71f57fdd37519
Denis Weerasiri <ddweerasiri@gmail.com>
2017-10-29 20:11:20 +1100

    Fixed the node runtime version to be installed in the target environment

--- code-deploy-scripts/after-install.sh
+++ code-deploy-scripts/after-install.sh
@@ -3,7 +3,7 @@
 echo "Running after-install.sh..."
 
 sudo yum install -y gcc-c++ make
-curl -sL https://rpm.nodesource.com/setup_6.x | sudo -E bash -
+curl -sL https://rpm.nodesource.com/setup_8.x | sudo -E bash -
 
 sudo yum install -y nodejs
 
