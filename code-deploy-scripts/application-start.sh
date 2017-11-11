8fc54d76fddf87993a119204f671fcfdd9dbf197
Denis Weerasiri <ddweerasiri@gmail.com>
2017-11-12 00:07:49 +1100

    Fixed some deployment errors (cloudfomration and code-deploy scripts)

--- code-deploy-scripts/application-start.sh
+++ code-deploy-scripts/application-start.sh
@@ -7,7 +7,7 @@ cd /home/ec2-user/ddweerasiri-website
 sudo rm -rf node_modules
 sudo npm install
 sudo npm run build
-sudo nohup npm run start &
+sudo nohup npm run start > /dev/null 2>&1 &
 sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 3000
 
 if [ $? -eq 0 ]
